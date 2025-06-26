d89abb99c0e577fcf7ffa7a8ba1b409aa3d91588
platipusica <babicdd@gmail.com>
2025-06-26 07:05:48 +0000

    comment from python script

--- conf.py
+++ conf.py
@@ -566,3 +566,7 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 
 #texinfo_no_detailmenu = False
+
+llms_txt_exclude = [
+    "requirements",  # Exclude the search page
+]
