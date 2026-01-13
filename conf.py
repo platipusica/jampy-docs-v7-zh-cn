31bd7281ec26bda480fdd9ccd2c8f2b49ea62af2
platipusica <dbabic@gmail.com>
2026-01-13 12:29:31 +0800

    rtm gh link

--- conf.py
+++ conf.py
@@ -52,7 +52,13 @@ import shlex
 extensions = ['sphinx_copybutton', 'sphinxcontrib.jquery']
 html_baseurl = 'https://jampy-docs.readthedocs.io/projects/V7/zh-cn/latest'
 
-
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "platipusica", # Username
+    "github_repo": "jampy-docs-v7-zh-cn", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "/", # Path in the checkout to the docs root
+}
 
 # Add any paths that contain templates here, relative to this directory.
 
